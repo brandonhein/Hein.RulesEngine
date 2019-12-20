ad901ba37307f8869c464883482d48e2f2c9c837
Hein, Brandon <BrandonHein@quickenloans.com>
2019-12-20 12:51:41 -0500

    dynamic boolean Hein.Framework.Extensions :heart:

---
+++ src/RulesEngine.Framework/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hein.RulesEngine.Framework
+{
+    public static class StringExtensions
+    {
+        public static bool IsOneOf(this string val, params string[] comparisonValue)
+        {
+            if (!string.IsNullOrEmpty(val))
+            {
+                foreach (var s in comparisonValue)
+                {
+                    if (!string.IsNullOrEmpty(s) &&
+                        s.Equals(val, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/RulesEngine.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hein.RulesEngine.Application.Execution;
+using Hein.Framework.Extensions;
+
+namespace Hein.RulesEngine.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var code = "\"3005\".IsOneOf(\"3005\",\"3010\")";
+            var result = code.Execute<bool>();
+
+            var extResult = "3005".IsOneOf("3005", "3010");
+
+            return Ok(result);
+        }
+    }
+}
